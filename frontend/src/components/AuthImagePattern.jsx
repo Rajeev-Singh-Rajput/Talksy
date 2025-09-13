745fc8619c794d1bfdce1f153e56cfc48b38c6a1
Rajeev Singh Rajput <ssrajput2244@gmail.com>
2025-09-13 22:25:03 +0530

    Fine Tuned frontend

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,26 @@
+const AuthImagePattern = ({ title, subtitle, isLogin = true }) => {
+  return (
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+      <div className="max-w-md text-center">
+        {/* <div className="grid grid-cols-3 gap-3 mb-8">
+          {[...Array(9)].map((_, i) => (
+            <div
+              key={i}
+              className={`aspect-square rounded-2xl bg-primary/10 ${
+                i % 2 === 0 ? "animate-pulse" : ""
+              }`}
+            />
+          ))}
+        </div> */}
+        <img
+          src={isLogin ? "/login.png" : "/signup.png"}
+          className="w-full h-auto object-contain"
+        />
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthImagePattern;
